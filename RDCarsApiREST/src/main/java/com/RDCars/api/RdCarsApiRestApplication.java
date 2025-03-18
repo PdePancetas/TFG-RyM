73d7f3f83ffd66c2b1ed502e38cc9a31386aabd3
RicardoRomero2003 <ricardo.romero9.2003@gmail.com>
2025-03-18 11:39:41 +0100

    primer commit apiREST

---
+++ RDCarsApiREST/src/main/java/com/RDCars/api/RdCarsApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.RDCars.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RdCarsApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RdCarsApiRestApplication.class, args);
+	}
+
+}
---
+++ RDCarsApiREST/src/test/java/com/RDCars/api/RdCarsApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RDCars.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RdCarsApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
