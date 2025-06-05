5a689a3f835b08c65f601e5e35428028bad78db2
PdePancetas <blancomiguel.bj@gmail.com>
2025-06-05 20:52:12 +0200

    a

--- DRCarsApiREST/src/main/java/com/DRCars/controller/AuthController.java
+++ DRCarsApiREST/src/main/java/com/DRCars/controller/AuthController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import com.DRCars.dto.LoginRequest;
 import com.DRCars.model.Cliente;
 import com.DRCars.model.Usuario;
+import com.DRCars.model.Usuario.TipoUsuario;
 import com.DRCars.service.impl.ClienteServiceImpl;
 import com.DRCars.service.impl.UsuarioServiceImpl;
 
@@ -31,20 +32,23 @@ public class AuthController {
 	public ResponseEntity<String> login(@RequestBody LoginRequest request) {
 		Optional<Usuario> usuario = usuarioService.obtenerUsuarioPorId(request.getUsuario());
 		if (!usuario.isEmpty()) {
-			Optional<Cliente> cliente = Optional.empty();
-			if (usuario.isPresent()
-					&& usuarioService.verificarContraseña(request.getContraseña(), usuario.get().getContraseña())) {
-				usuario.get().setUltimo_acceso(request.getUltimo_acceso());
-				usuarioService.crearUsuario(usuario.get());
+			if (usuario.get().getTipoUsuario().equals(TipoUsuario.ADMIN)) {
+				Optional<Cliente> cliente = Optional.empty();
+				if (usuario.isPresent()
+						&& usuarioService.verificarContraseña(request.getContraseña(), usuario.get().getContraseña())) {
+					usuario.get().setUltimo_acceso(request.getUltimo_acceso());
+					usuarioService.crearUsuario(usuario.get());
 
-				cliente = clientService.obtenerClientePorEmail(request.getUsuario());
-				if (cliente.isPresent())
-					return ResponseEntity.ok("Autenticación exitosa, " + usuario.get().getTipoUsuario()
-							+ " cliente ha iniciado sesión: " + cliente.get().getDniCliente());
-				else
-					return ResponseEntity.ok("Autenticación exitosa, " + usuario.get().getTipoUsuario()
-							+ " usuario ha iniciado sesión: " + usuario.get().getUsuario());
+					cliente = clientService.obtenerClientePorEmail(request.getUsuario());
+					if (cliente.isPresent())
+						return ResponseEntity.ok("Autenticación exitosa, " + usuario.get().getTipoUsuario()
+								+ " cliente ha iniciado sesión: " + cliente.get().getDniCliente());
+					else
+						return ResponseEntity.ok("Autenticación exitosa, " + usuario.get().getTipoUsuario()
+								+ " usuario ha iniciado sesión: " + usuario.get().getUsuario());
+				}
 			}
+			return ResponseEntity.status(403).body("Acceso denegado. No tienes permisos de administrador.");
 		}
 		return ResponseEntity.status(401).body("Credenciales incorrectas, no ha podido iniciar sesión");
 	}
