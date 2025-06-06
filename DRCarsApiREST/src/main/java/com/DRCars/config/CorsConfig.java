c75d209c223d67d716726e237d3962b6089d8f91
PdePancetas <blancomiguel.bj@gmail.com>
2025-06-06 19:02:55 +0200

    Añadir localhost 3000 a la lista de origenes aceptados por la api

--- DRCarsApiREST/src/main/java/com/DRCars/config/CorsConfig.java
+++ DRCarsApiREST/src/main/java/com/DRCars/config/CorsConfig.java
@@ -8,14 +8,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
     @Bean
-    public WebMvcConfigurer corsConfigurer() {
+    WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                    .allowedOrigins("https://v0-pagina-de-venta-drc-ars.vercel.app/",
-                    		"https://localhost:3000/")
-                    .allowedMethods("*")
+                    .allowedOrigins("https://v0-pagina-de-venta-drc-ars.vercel.app/**",
+                    		"https://localhost:3000/**")
+                    .allowedMethods("GET","POST")
                     .allowedHeaders("*");
             }
         };
