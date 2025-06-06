e14121ad70d470ce91744e7784b2139e79acb971
PdePancetas <blancomiguel.bj@gmail.com>
2025-06-06 18:51:19 +0200

    Añadir localhost 3000 a la lista de origenes aceptados por la api

--- DRCarsApiREST/src/main/java/com/DRCars/config/CorsConfig.java
+++ DRCarsApiREST/src/main/java/com/DRCars/config/CorsConfig.java
@@ -13,7 +13,8 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                    .allowedOrigins("https://v0-pagina-de-venta-drc-ars.vercel.app/")
+                    .allowedOrigins("https://v0-pagina-de-venta-drc-ars.vercel.app/",
+                    		"https://localhost:3000/")
                     .allowedMethods("*")
                     .allowedHeaders("*");
             }
